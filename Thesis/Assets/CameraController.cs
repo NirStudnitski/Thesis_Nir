91e30ea62b52e06ee04383b03ce8e4fc55bb115a
NirStudnitski <nir.stud@gmail.com>
2016-01-25 13:01:59 -0800

    gui elements

---
+++ Thesis/Assets/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public void ChangeDistance(float fIn)
+	{
+		this.transform.position = new Vector3 (-fIn, -3.4f +fIn, -fIn);
+
+
+	}
+}
